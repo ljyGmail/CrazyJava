a7fd9c1aff653ad820bcfcf40fa141570eae4e53
ljy <liangjy881@naver.com>
2023-03-31 00:13:36 +0900

    开始面向对象

---
+++ src/main/java/org/example/ch05_oop1/sec_01/A_Person.java
@@ -0,0 +1,12 @@
+package org.example.ch05_oop1.sec_01;
+
+public class A_Person {
+    // 下面定义了两个成员变量
+    public String name;
+    public int age;
+
+    //下面定义了一个say方法
+    public void say(String content) {
+        System.out.println(content);
+    }
+}
