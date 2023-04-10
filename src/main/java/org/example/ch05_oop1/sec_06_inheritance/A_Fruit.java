cf5805098088f5ed71eecc104827edcaf64b98a0
ljy <liangjy881@naver.com>
2023-04-10 19:11:07 +0900

    开始5.6: 类的继承

---
+++ src/main/java/org/example/ch05_oop1/sec_06_inheritance/A_Fruit.java
@@ -0,0 +1,9 @@
+package org.example.ch05_oop1.sec_06_inheritance;
+
+public class A_Fruit {
+    public double weight;
+
+    public void info() {
+        System.out.println("我是一个水果！重" + weight + "g!");
+    }
+}
