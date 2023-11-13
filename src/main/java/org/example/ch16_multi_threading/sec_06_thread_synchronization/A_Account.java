bdbcc39ce7fde90e2628d45534e65b3e88720011
liangjinyong <liangjy881@gmail.com>
2023-11-13 16:48:23 +0900

    16.5.1 线程安全问题

---
+++ src/main/java/org/example/ch16_multi_threading/sec_06_thread_synchronization/A_Account.java
@@ -0,0 +1,49 @@
+package org.example.ch16_multi_threading.sec_06_thread_synchronization;
+
+public class A_Account {
+    // 封装账户编号、账户余额的两个成员变量
+    private String accountNo;
+    private double balance;
+
+    // 构造器
+    public A_Account() {
+    }
+
+    public A_Account(String accountNo, double balance) {
+        this.accountNo = accountNo;
+        this.balance = balance;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    // 下面两个方法根据accountNo来重写hashCode()和equals()方法
+    @Override
+    public int hashCode() {
+        return accountNo.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj != null && obj.getClass() == A_Account.class) {
+            var target = (A_Account) obj;
+            return target.getAccountNo().equals(accountNo);
+        }
+        return false;
+    }
+}
---
+++ src/main/java/org/example/ch16_multi_threading/sec_06_thread_synchronization/B_DrawThread.java
@@ -0,0 +1,34 @@
+package org.example.ch16_multi_threading.sec_06_thread_synchronization;
+
+public class B_DrawThread extends Thread {
+    // 模拟用户账户
+    private A_Account account;
+    // 当前取钱线程所希望取的钱数
+    private double drawAmount;
+
+    public B_DrawThread(String name, A_Account account, double drawAmount) {
+        super(name);
+        this.account = account;
+        this.drawAmount = drawAmount;
+    }
+    // 当多个线程修改同一个共享数据时，将涉及数据安全问题
+
+    @Override
+    public void run() {
+        // 账户余额大于取钱数目
+        if (account.getBalance() >= drawAmount) {
+            // 吐出钞票
+            System.out.println("取钱成功！吐出钞票: " + drawAmount);
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            // 修改余额
+            account.setBalance(account.getBalance() - drawAmount);
+            System.out.println("\t余额为: " + account.getBalance());
+        } else {
+            System.out.println(getName() + "取钱失败！余额不足！");
+        }
+    }
+}
